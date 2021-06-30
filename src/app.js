ee96a61cbf3513b6818b46859ec6a6a530b337e7
Planetcare suprimentos <juliamariah.net@gmail.com>
2021-06-29 23:51:17 -0300

    criando servidor do projeto final

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express") // 1 - isso eu importei do express
+
+const app = express() // 2 - criei o app
+
+module.exports = app // 3 - aqui estou exportando o APP e suas dependencias(express) para o server.js
+
+
+
