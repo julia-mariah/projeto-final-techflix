cf8b79a4c1ab45c71d00fb473630e84c3b8f1959
Planetcare suprimentos <juliamariah.net@gmail.com>
2021-07-09 23:16:09 -0300

    update rota incial projeto final

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
     res.status(200).json({
-        message: "Olá !! Aqui você vai encontrar muita ajuda para quem está inciando no unverso do TI",
+        message: "Olá !! Aqui você vai encontrar muita ajuda e informação para conhecer o universo Dev e TI",
         version: 1.0,
-        autor: "Julia Mariah",
-        site: "www.techflix.com.br"
+        autor: "Julia Mariah Elyseu",
+        site: "www.techflix.com.br",
+        date: 2021,
+        descricao: "Projeto final do curso da REPROGRAMA"
 
     })
 })
